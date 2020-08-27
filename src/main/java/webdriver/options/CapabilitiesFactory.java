98440d127fe230f1d8bec57cc7a234e807c22451
isandratskiy <i.sandrackii@gmail.com>
2020-08-27 19:20:57 +0300

    clean up

--- src/main/java/webdriver/options/CapabilitiesFactory.java
+++ src/main/java/webdriver/options/CapabilitiesFactory.java
@@ -2,6 +2,8 @@ package webdriver.options;
 
 import org.openqa.selenium.Capabilities;
 
+import static com.codeborne.selenide.WebDriverRunner.CHROME;
+import static com.codeborne.selenide.WebDriverRunner.FIREFOX;
 import static java.lang.System.getProperty;
 
 public final class CapabilitiesFactory {
@@ -12,16 +14,16 @@ public final class CapabilitiesFactory {
 
     public static Capabilities getDriverCapabilities() {
         switch (getBrowserProperty()) {
-            case "chrome":
+            case CHROME:
                 return Option.CHROME.get();
-            case "firefox":
+            case FIREFOX:
                 return Option.FIREFOX.get();
             default: throw new IllegalStateException("Incorrect browser type: " + getBrowserProperty());
         }
     }
 
     private static String getBrowserProperty() {
-        return getProperty(BROWSER_PROPERTY, "chrome");
+        return getProperty(BROWSER_PROPERTY, CHROME);
     }
 
     private enum Option {
